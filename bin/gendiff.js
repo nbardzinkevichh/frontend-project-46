bfeb26e2225d8a0bf73bcaba091dceb98ffa7e65
nikolai.b <nbardzinkevichh@gmail.com>
2024-08-10 19:34:51 +0800

    add different ext parse to input files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,26 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import path from 'node:path';
+import { readFileSync } from 'node:fs';
+import parseFiles from '../src/parseFiles.js';
+
+const normalizedPath = (filePath) => (filePath.startsWith('../') ? filePath.replace('../', '') : filePath);
+const defineFormat = (file) => {
+  const fileExt = path.parse(file).ext;
+  return fileExt;
+};
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .argument('<filepath1>')
   .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    const file1Data = readFileSync(normalizedPath(filepath1));
+    const file2Data = readFileSync(normalizedPath(filepath2));
+    const fileExt = defineFormat(filepath1);
+    parseFiles(file1Data, file2Data, fileExt);
+  })
   .option('-f, --format [type]', 'output format')
   .parse(process.argv);
---
+++ src/parseFiles.js
@@ -0,0 +1,14 @@
+import { parse as yamlParse, stringify } from 'yaml';
+
+const parseFiles = (file1, file2, ext) => {
+  if (ext === '.json') {
+    const parsedFile1Data = JSON.parse(file1);
+    const parsedFile2Data = JSON.parse(file2);
+    console.log(parsedFile1Data, parsedFile2Data);
+  } else if (ext === '.yaml') {
+    const parsedFile1Data = yamlParse(file1);
+    const parsedFile2Data = yamlParse(file2);
+    console.log(parsedFile1Data, parsedFile2Data);
+  }
+};
+export default parseFiles;
