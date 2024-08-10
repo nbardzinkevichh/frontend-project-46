ce71f595084fb8bfa2732bc2a0e108ba6908c621
nikolai.b <nbardzinkevichh@gmail.com>
2024-08-10 17:01:35 +0800

    add arguments and option to cli program

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,8 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
-program.parse();
+  .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .parse(process.argv);
