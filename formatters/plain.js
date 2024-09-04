2fbec8848070233117b7c930cb0dd0063033b3a6
nikolai.b <nbardzinkevichh@gmail.com>
2024-09-04 17:41:21 +0800

    fix plain formatter

--- formatters/plain.js
+++ formatters/plain.js
@@ -4,7 +4,7 @@ const getComplexValue = (value) => {
   if (_.isObject(value)) {
     return '[complex value]';
   }
-  if (typeof value === 'boolean' || value === null) {
+  if (typeof value === 'boolean' || value === null || typeof value === 'number') {
     return value;
   }
   return `'${value}'`;
