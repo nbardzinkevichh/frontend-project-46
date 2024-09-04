e10abe525b6a948faafa4513e238cc973f03cbeb
nikolai.b <nbardzinkevichh@gmail.com>
2024-09-04 18:45:19 +0800

    add jest cli package

--- formatters/plain.js
+++ formatters/plain.js
@@ -14,17 +14,17 @@ const plain = (data) => {
   const formatter = (tree, path) => {
     // eslint-disable-next-line array-callback-return, consistent-return
     const styledData = tree.flatMap((object) => {
-      if (object.type === 'added') {
-        return `Property '${path}${object.key}' was added with value: ${getComplexValue(object.value)}`;
-      }
-      if (object.type === 'deleted') {
-        return `Property '${path}${object.key}' was removed`;
-      }
-      if (object.type === 'changed') {
-        return `Property '${path}${object.key}' was updated. From ${getComplexValue(object.firstValue)} to ${getComplexValue(object.secondValue)}`;
-      }
-      if (object.type === 'hasChild') {
-        return formatter(object.value, `${path}${object.key}.`);
+      switch (object.type) {
+        case 'added':
+          return `Property '${path}${object.key}' was added with value: ${getComplexValue(object.value)}`;
+        case 'deleted':
+          return `Property '${path}${object.key}' was removed`;
+        case 'changed':
+          return `Property '${path}${object.key}' was updated. From ${getComplexValue(object.firstValue)} to ${getComplexValue(object.secondValue)}`;
+        case 'hasChild':
+          return formatter(object.value, `${path}${object.key}.`);
+        default:
+          break;
       }
     });
     return [
